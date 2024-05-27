df31dc3bade7fe5a357dea2ad0c34ac6cb72abcf
GalPaz <galpaz2210@gmail.com>
2024-05-27 15:11:21 +0300

    loading to login4

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -57,7 +57,7 @@ export default function LoginPage() {
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
           Login
         </button>
-        {loginloading && <p className="mt-4 text-white text">Loading...</p>}
+        {loginloading && <p className="mt-4 ">Loading...</p>}
       </form>
     </div>
   );
