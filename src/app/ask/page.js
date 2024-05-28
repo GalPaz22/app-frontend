4c2e3718e8e4d692bfa9cf68d7a072fff15c9abe
GalPaz <galpaz2210@gmail.com>
2024-05-28 23:27:10 +0300

    session- try24

--- src/app/ask/page.js
+++ src/app/ask/page.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
-import { headers } from "next/headers";
+
 
 const API_URL = "https://app-backend-urlo.onrender.com"; // Adjust this URL to your backend
 
@@ -121,25 +121,25 @@ export default function Home() {
     }
   };
 
-  const handleLogout = async () => {
-    try {
-      const userId = Cookies.get("userId");
-  
-      await axiosInstance.get("/logout",  {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userId}`,
-        },
-      });
-  
-      Cookies.remove("userId");
-      setAuthenticated(false);
-      router.push("/login");
-    } catch (error) {
-      console.error("Error during logout:", error);
-    }
-  };
-  
+ const handleLogout = async () => {
+  try {
+    const userId = Cookies.get("userId");
+
+    await axiosInstance.post("/logout", {}, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userId}`,
+      },
+    });
+
+    Cookies.remove("userId");
+    setAuthenticated(false);
+    router.push("/login");
+  } catch (error) {
+    console.error("Error during logout:", error);
+  }
+};
+
 
   if (authenticated === null) {
     return (
