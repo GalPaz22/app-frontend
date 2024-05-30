19153e59cf9fa39824d48ec451fcac08ba865758
GalPaz <galpaz2210@gmail.com>
2024-05-30 16:19:44 +0300

    getting ready12

--- src/app/ask/page.js
+++ src/app/ask/page.js
@@ -213,7 +213,7 @@ export default function Home() {
         </form>
         {loading && <div className="mt-4 text-gray-700">Loading...</div>}
         <div id="response-container" className="mt-4">
-          {conversation.map((entry, index) => (
+          {conversation.reverse().map((entry, index) => (
             <div
               key={index}
               className={`p-2 border rounded-md mb-2 ${
