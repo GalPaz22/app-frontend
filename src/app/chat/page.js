b8aafbf35c58ae6ae108fb475ca67bace03b2694
GalPaz <galpaz2210@gmail.com>
2024-06-04 21:59:26 +0300

    navbar+gpt51

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
-import Navbar from "./Navbar";
+import Navbar from "../Navbar";
 
 const API_URL = "https://app-backend-urlo.onrender.com"; // Adjust this URL to your backend
 
