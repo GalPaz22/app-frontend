63a14e1d306d99fb606b51f56c6ca767b20f013f
GalPaz <galpaz2210@gmail.com>
2024-06-04 23:26:26 +0300

    navbar+gpt53

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import Navbar from "../Navbar";
 
