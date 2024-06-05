9f8174c40392a5b825ce3417d7a8737046ccf3dc
GalPaz <galpaz2210@gmail.com>
2024-06-05 23:25:04 +0300

    navbar+gpt101

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -99,7 +99,7 @@ export default function Chat() {
 
         for (const line of lines) {
           if (line.startsWith('data:')) {
-            const data = JSON.parse(line.substring(5));
+            const data =(line.substring(5));
             if (data.content !== '[DONE]') {
               setGeneration((currentGeneration) => currentGeneration + data.content);
             }
