0e31e8b0f30d92db0813582a46938facb6393449
GalPaz <galpaz2210@gmail.com>
2024-06-06 13:19:37 +0300

    new1

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -88,7 +88,7 @@ export default function Chat() {
         const chunk = decoder.decode(value);
          console.log(chunk);
         
-        setGeneration((prevGeneration) => prevGeneration + chunk);
+        setGeneration((prevGeneration) => prevGeneration + chunk.text);
 
         if (done) {
           break;
