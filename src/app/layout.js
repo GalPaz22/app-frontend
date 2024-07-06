3331c411ae3cd965e46e6442b4ae5f99a8d96277
GalPaz <galpaz2210@gmail.com>
2024-07-06 10:54:36 +0300

    new2

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2790217851333052"
+     crossorigin="anonymous"></script>
+
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
