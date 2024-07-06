e9e5f728f64b191711a49e1bd6ac6f014fa465dd
GalPaz <galpaz2210@gmail.com>
2024-07-06 14:42:24 +0300

    new2

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,17 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="\iconmonstr-file-34.svg"></link>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2790217851333052"
      crossorigin="anonymous"></script>
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XQHNQGN6SY"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XQHNQGN6SY');
-</script>
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XQHNQGN6SY"></script>
    
      
 
