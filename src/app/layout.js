283d9ba6e4f1f44ba76598ac14fc90e769309a01
GalPaz <galpaz2210@gmail.com>
2024-07-06 14:24:26 +0300

    new2

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "100% Free ChatPDF- no registration required",
   description: "Poor UI, High performance.",
+  keywords: "chatbot, pdf, free, no registration required",
+  
 };
 
 export default function RootLayout({ children }) {
@@ -15,7 +17,16 @@ export default function RootLayout({ children }) {
       <head>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2790217851333052"
      crossorigin="anonymous"></script>
-     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XQHNQGN6SY"></script>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XQHNQGN6SY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XQHNQGN6SY');
+</script>
+   
      
 
       </head>
