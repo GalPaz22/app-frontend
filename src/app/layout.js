7c44d370320ec6ebb42789c4ac9e2e9c30daf443
GalPaz <galpaz2210@gmail.com>
2024-07-08 14:57:46 +0300

    new2

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-XQHNQGN6SY"></script>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
