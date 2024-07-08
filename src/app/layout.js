84f061a75a935e267b5fb7ad9ff89977f1d7f0bc
GalPaz <galpaz2210@gmail.com>
2024-07-08 10:17:36 +0300

    new2

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,19 +7,19 @@ import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: {
-    en: "100% Free ChatPDF - no registration required",
-    he: "ChatPDF חינם לגמרי - ללא רישום",
-  },
-  description: {
-    en: "Poor UI, High performance.",
-    he: "ממשק גרוע, ביצועים גבוהים.",
-  },
-  keywords: {
-    en: "chatbot, pdf, free, no registration required",
-    he: "צ'אטבוט, pdf, חינם, ללא רישום",
-  }
+const titles = {
+  en: "100% Free ChatPDF - no registration required",
+  he: "ChatPDF חינם לגמרי - ללא רישום",
+};
+
+const descriptions = {
+  en: "Poor UI, High performance.",
+  he: "ממשק גרוע, ביצועים גבוהים.",
+};
+
+const keywords = {
+  en: "chatbot, pdf, free, no registration required",
+  he: "צ'אטבוט, pdf, חינם, ללא רישום",
 };
 
 export default function RootLayout({ children }) {
@@ -37,9 +37,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang={language}>
       <Head>
-        <title>{metadata.title[language]}</title>
-        <meta name="description" content={metadata.description[language]} />
-        <meta name="keywords" content={metadata.keywords[language]} />
+        <title>{titles[language]}</title>
+        <meta name="description" content={descriptions[language]} />
+        <meta name="keywords" content={keywords[language]} />
         <link rel="icon" href="/iconmonstr-file-34.svg" />
         <script async="async" data-cfasync="false" src="//pl23722004.highrevenuenetwork.com/e8ecdde516e1ce6931b965d75fbec896/invoke.js"></script>
       </Head>
